da4b0bca70646218172bbb496d85074079fb3088
cristobalcontrerasepys <elementalreggae@gmail.com>
2020-04-19 23:18:08 -0400

    Fix

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,23 +1,22 @@
 /* eslint-disable */
 export const displayMap = locations => {
   mapboxgl.accessToken =
-    'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
-
+    'pk.eyJ1IjoiY2hyaXNsb2FycnluIiwiYSI6ImNrOHdwMDkxcTBwcG8zbG1raTA2bGNicncifQ.HnHqUlOlMYtWNQNEgk2K4Q'
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
+    style: 'mapbox://styles/chrisloarryn/ck8wp5zvv22ps1ivs5p1te7jz',
     scrollZoom: false
-    // center: [-118.113491, 34.111745],
-    // zoom: 10,
-    // interactive: false
-  });
+    //   center: [-118.803461, 34.006072],
+    //   zoom: 10,
+    //   interactive: false
+  })
 
-  const bounds = new mapboxgl.LngLatBounds();
+  const bounds = new mapboxgl.LngLatBounds()
 
   locations.forEach(loc => {
     // Create marker
-    const el = document.createElement('div');
-    el.className = 'marker';
+    const el = document.createElement('div')
+    el.className = 'marker'
 
     // Add marker
     new mapboxgl.Marker({
@@ -25,7 +24,7 @@ export const displayMap = locations => {
       anchor: 'bottom'
     })
       .setLngLat(loc.coordinates)
-      .addTo(map);
+      .addTo(map)
 
     // Add popup
     new mapboxgl.Popup({
@@ -33,11 +32,11 @@ export const displayMap = locations => {
     })
       .setLngLat(loc.coordinates)
       .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-      .addTo(map);
+      .addTo(map)
 
     // Extend map bounds to include current location
-    bounds.extend(loc.coordinates);
-  });
+    bounds.extend(loc.coordinates)
+  })
 
   map.fitBounds(bounds, {
     padding: {
@@ -46,5 +45,5 @@ export const displayMap = locations => {
       left: 100,
       right: 100
     }
-  });
-};
+  })
+}

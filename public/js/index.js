a53266016b341af7903d66ee698f2dd676a71791
cristobalcontrerasepys <elementalreggae@gmail.com>
2020-04-19 21:50:08 -0400

    Fix

--- public/js/index.js
+++ public/js/index.js
@@ -68,5 +68,5 @@ if (bookBtn) {
   })
 }
 
-const alertMessage = document.querySelector('body').dataset.alertMessage
+const alertMessage = document.querySelector('body').dataset.alert
 if (alertMessage) showAlert('success', alertMessage, 20)

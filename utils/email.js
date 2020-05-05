69a14934fa24b5d5450049d8b01540fd0f71df5f
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-05 19:01:41 -0400

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,7 @@ module.exports = class Email {
   }
 
   newTransport () {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'productions') {
       // SendGrid for sending emails
       return nodemailer.createTransport({
         service: 'SendGrid',
